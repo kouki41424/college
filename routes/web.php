5cc4480a7cd2e62c3a8dbf908f6ce37a1a421718
EC2 Default User <ec2-user@ip-172-31-68-125.ec2.internal>
2022-10-16 14:49:39 +0900

    カリキュラム08-2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Postcontroller;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class,'index']); 
+    
+
