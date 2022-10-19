3b6313f811e391769ddde1e8359564171c30f363
EC2 Default User <ec2-user@ip-172-31-68-125.ec2.internal>
2022-10-19 20:57:55 +0900

    カリキュラム08-4

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/',[PostController::class,'index']);
+Route::get('/posts/create',[PostController::class,'create']);
+Route::post('/posts',[PostController::class,'store']);
+Route::get('/posts/{post}',[PostController::class, 'show']);
+
